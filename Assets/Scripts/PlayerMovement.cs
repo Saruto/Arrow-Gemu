c077d5e25cd6a4e3ca98f1f5aee08ca4b4b63234
Saruto <saruto713@gmail.com>
2018-06-05 18:54:57 -0400

    uses character controller, player rotates based on input/velocity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,25 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Components on this Character Gameobject
 	Rigidbody rb;
+	CharacterController charController;
 
-	// Continuous forces to apply to the character.
-	// Set in update and used in fixed update
-	Vector3 ForceToApply;
+	Vector3 moveDirection = Vector3.zero;
+
+	// The camera's forwards and right vectors, parallel to the XZ plane
+	Vector3 cameraPlanarForwards {
+		get {
+			Vector3 camFwd = Camera.transform.forward;
+			camFwd.y = 0;
+			return camFwd.normalized;
+		}
+	}
+	Vector3 cameraPlanarRight {
+		get {
+			Vector3 camRight = Camera.transform.right;
+			camRight.y = 0;
+			return camRight.normalized;
+		}
+	}
 
 
 	//  --------- Serialized Fields ---------  //
@@ -20,20 +35,16 @@ public class PlayerMovement : MonoBehaviour {
 	[SerializeField] GameObject Camera;
 
 	// Speed of movement
-	[SerializeField] float MovementForce;
-
-	// Max Velocity
-	[SerializeField] float MaxVelocity;
+	[SerializeField] float MovementSpeed;
 
 	// Speed of movement
-	[SerializeField] float JumpingForce;
+	[SerializeField] float JumpingSpeed;
 
 	// Speed of rotation
 	[SerializeField] float RotationSpeed;
 
 	// Gravity Scale
-	[Range(1f, 100f)]
-	[SerializeField] float GravityScale;
+	[SerializeField] float Gravity;
 
 	// ------------------------------------------ Methods ------------------------------------------ //
 
@@ -41,70 +52,74 @@ public class PlayerMovement : MonoBehaviour {
 	//  --------- Start ---------  //
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		charController = GetComponent<CharacterController>();
 	}
 	
 	//  --------- Update ---------  //
 	void Update () {
-		ForceToApply = Vector3.zero;
 		// --- Simple Movement --- //
-
-		// Rotates the camera's forwards/right unit vectors so that they're parallel to the XZ plane.
-		Vector3 cameraPlanarForwards = Camera.transform.forward;
-		cameraPlanarForwards.y = 0;
-		cameraPlanarForwards.Normalize();
-		Vector3 cameraPlanarRight = Camera.transform.right;
-		cameraPlanarRight.y = 0;
-		cameraPlanarRight.Normalize();
+		if(charController.isGrounded){
+			moveDirection = Vector3.zero;
+		} else {
+			moveDirection = new Vector3(0f, moveDirection.y, 0f);
+		}
 
 		// Forwards/Backwards
 		if(Input.GetKey(KeyCode.W)) {
-			ForceToApply += cameraPlanarForwards;
+			moveDirection += cameraPlanarForwards;
 		} else if(Input.GetKey(KeyCode.S)) {
-			ForceToApply += -cameraPlanarForwards;
+			moveDirection += -cameraPlanarForwards;
 		}
 
 		// Right/Left
 		if(Input.GetKey(KeyCode.D)) {
-			ForceToApply += cameraPlanarRight;
+			moveDirection += cameraPlanarRight;
 		} else if(Input.GetKey(KeyCode.A)) {
-			ForceToApply += -cameraPlanarRight;
+			moveDirection += -cameraPlanarRight;
 		}
 
 		// Set the magnitude
-		ForceToApply = ForceToApply.normalized * MovementForce;
-
+		moveDirection.x *= MovementSpeed;
+		moveDirection.z *= MovementSpeed;
 
 		// --- Jumping --- //
-		if(Input.GetKeyDown(KeyCode.Space)) {
-			rb.AddForce(transform.up * JumpingForce, ForceMode.Impulse);
-			ForceToApply *= 0.2f;
+		if(Input.GetKeyDown(KeyCode.Space) && charController.isGrounded) {
+			moveDirection.y = JumpingSpeed;
 		}
 
+		// --- Applying Movement --- //
+		moveDirection.y -= Gravity * Time.deltaTime;
+		charController.Move(moveDirection * Time.deltaTime);
 
 		// --- Rotation --- //
-		//RotateTowardsMovement();
-	}
-
-	//  --------- FixedUpdate ---------  //
-	void FixedUpdate() {
-		// Add Forces
-		rb.AddForce(ForceToApply);
-
-		// Add Additional Gravity
-		rb.AddForce(-1 * (1f - GravityScale) * (Physics.gravity * rb.mass));
-
-		// Cap Planar Velocities
-		Vector3 planeSpeed = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-		if(planeSpeed.sqrMagnitude > MaxVelocity * MaxVelocity) {
-			planeSpeed = planeSpeed.normalized * MaxVelocity;
-			rb.velocity = new Vector3(planeSpeed.x, rb.velocity.y, planeSpeed.z);
-		}
+		RotateTowardsMovement();
 	}
 
 	// Rotates the player towards the direction they're moving.
 	void RotateTowardsMovement() {
 		float step = RotationSpeed * Time.deltaTime;
-		Vector3 newDir = Vector3.RotateTowards(transform.forward, rb.velocity, RotationSpeed, 0f);
+		
+		// Get movement direction vector. Based on velocity and input direction.
+		Vector3 facingDirection = Vector3.zero;
+		// X and Z components based on input
+		facingDirection += Input.GetKey(KeyCode.W) ? cameraPlanarForwards : Input.GetKey(KeyCode.S) ? -cameraPlanarForwards : Vector3.zero;
+		facingDirection += Input.GetKey(KeyCode.D) ? cameraPlanarRight : Input.GetKey(KeyCode.A) ? -cameraPlanarRight : Vector3.zero;
+		// Y component based on velocity
+		facingDirection.y = charController.velocity.y;
+		facingDirection.y /= 15f;
+
+		// If the character isn't moving, make them face the direction they're already facing without the y component.
+		if(facingDirection == Vector3.zero || (facingDirection.x == 0f && facingDirection.z == 0f)) {
+			facingDirection = transform.forward;
+			facingDirection.y = 0f;
+		}
+
+		// If the character is grounded, get rid of the y rotation.
+		else if(charController.isGrounded) {
+			facingDirection.y = 0f;
+		}
+
+		Vector3 newDir = Vector3.RotateTowards(transform.forward, facingDirection, RotationSpeed, 0f);
 		transform.rotation = Quaternion.LookRotation(newDir);
 	}
 
